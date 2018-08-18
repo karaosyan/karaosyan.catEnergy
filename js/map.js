c166cd041d8143e6d00b014923c9716eeceeeb12
Karen Osian <karaosyan@gmail.com>
2018-08-18 17:12:58 +0300

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,71 @@
+var bigPin = {width: 124, height: 106};
+var smallPin = {width: 62, height: 53};
+
+function initMap() {
+  if(document.body.clientWidth < 768) {
+    var mapCenter = {lat: 59.938845, lng: 30.323200};
+    var pinCenter = {lat: 59.938800, lng: 30.323200};
+    var iconSize = smallPin;
+
+  } else if (document.body.clientWidth >= 768 && document.body.clientWidth < 1300) {
+    var mapCenter = {lat: 59.939085, lng: 30.323200};
+    var pinCenter = {lat: 59.938800, lng: 30.323200};
+    var iconSize = bigPin;
+
+  } else if (document.body.clientWidth >= 1300) {
+    var mapCenter = {lat: 59.939095, lng: 30.319530};
+    var pinCenter = {lat: 59.938800, lng: 30.323200};
+    var iconSize = bigPin;
+  }
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: mapCenter
+  });
+
+  var image = new google.maps.MarkerImage(
+    'img/map-pin.png',
+    null,
+    null,
+    null,
+    iconSize
+  );
+
+  var marker = new google.maps.Marker({
+    position: pinCenter,
+    map: map,
+    icon: image
+  });
+
+  google.maps.event.addDomListener(window, 'resize', function() {
+    if(document.body.clientWidth >= 768) {
+      marker.setIcon(
+        new google.maps.MarkerImage(
+          marker.getIcon().url,
+          null,
+          null,
+          null,
+          bigPin
+        )
+      );
+    } else {
+      marker.setIcon(
+        new google.maps.MarkerImage(
+          marker.getIcon().url,
+          null,
+          null,
+          null,
+          smallPin
+        )
+      );
+    }
+
+    if (document.body.clientWidth >= 1300) {
+      map.setCenter(new google.maps.LatLng(59.939095, 30.319530));
+    } else if (document.body.clientWidth < 768) {
+      map.setCenter(new google.maps.LatLng(59.938845, 30.323200));
+    } else {
+      map.setCenter(new google.maps.LatLng(59.939085, 30.323200));
+    }
+  });
+}
